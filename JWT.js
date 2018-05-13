1014e03392ca0db6918aaaeb7bcb45be7bfefd7f
Jimmy <iamjimmyjimmy@yahoo.com>
2018-05-13 16:05:33 -0700

    Refactored Server, and implemented login, registration, and reset password
    Allows the user on SSO to register, reset password, or login, and cleaned up backend to be more easily read for the more code illiterate people

--- JWT.js
+++ JWT.js
@@ -1,100 +1,41 @@
-//get express framework
+// Get express framework
 var express = require('express');
 var app = express();
-//get bodyparser middleware
+
+// Get middleware modules
 var bodyParser = require('body-parser');
 var CryptoJS = require('crypto-js');
 var jwt = require('jsonwebtoken');
 
-// var x = {
-//     "username" : "Lazerman77777",
-//     "password" : "Lazerman7",
-//     "application" : "careaway",
-//     "roleType" : "public"
-// }
-
 app.use(bodyParser.urlencoded({
     extended: true
-  }));
-
-app.get('/gimmeJWT', function(req,res){
-    var x = {
-        "username" : "TheChosenOne12",
-        "password" : "777Lazer777",
-        "application" : "careaway",
-        "roleType" : "public" 
-    }
-    var token = jwt.sign(x,"db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==");
-    console.log("HERE");
-    res.end(token);
-})
-
-
+}));
+/**
+ * This is a GET request to get index.html file
+ */
 app.get('/', function (req, res) {
-    // var x = {
-    //     "username" : "isbae",
-    //     "password" : "Lazerman7",
-    //     "application" : "careaway",
-    //     "roleType" : "public"
-    // }
-    // var x = {
-    //     "username" : "AhriIsBae",
-    //     "password" : "Lazerman7",
-    //     "application" : "careaway",
-    //     "roleType" : "public" 
-    // }
-    // var x = {
-    //     "username" : "TheChosenOne12",
-    //     "password" : "777Lazer777",
-    //     "application" : "careaway",
-    //     "roleType" : "private"       
-    // }
-    // var x = {
-    //     "username" : "Test1234",
-    //     "password" : "Test1234Test1234Test1234",
-    //     "application" : "careaway",
-    //     "roleType" : "private"
-    // }
-    var x = {
-        "username" : "777Lazer777",
-        "password" : "7777Lazer7777",
-        "application" : "careaway",
-        "roleType" : "public" 
-    }
-    var token = jwt.sign(x,"db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==");
-    console.log(token);
-    //     var token = jwt.sign({ } ,"2disbetterthan3d");
-
-    //console.log(token);
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8085');
     res.sendFile( __dirname + "/" + "Index.html" );
 });
-
-app.post('/process', function(req,res){
-    var headtoken = req.headers.token;
-    var token = req.body.jwt;
-    console.log(headtoken);
-    jwt.verify(headtoken,"db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==",function(err, decode){
-        if(err){
-            console.log("BAD");
-        }
-        else{
-            var x= jwt.decode(headtoken,{complete: true});
-           // console.log(x.header);
-            //console.log(x.payload);
-            //console.log(x.payload.username);
-            var newUser = x.payload;
-            console.log(newUser);
-        }
-    });
-    res.end('SUCCESS');
+/**
+ * This is a GET request to get a Json Web Token
+ */
+app.get('/gimmeJWT', function(req,res){
+    // Generates a user object with username, password, application being sent to, and roletype
+    var user = {
+        "username" : req.query.username,
+        "password" : req.query.password,
+        "application" : "careaway",
+        "roleType" : "public" 
+    };
+    // Generates the Json Web Token
+    var token = jwt.sign(user,"db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==");
+    res.end(token);
 });
 
-
-
-
 var server = app.listen(8085, function () {
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
   console.log("Example app listening at http://%s:%s", host, port)
 
  });
